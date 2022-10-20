e6575bc989b6db4b7f60c2f90c42db0743f9ca38
Marie Clémensat <marieclemensat@wanadoo.fr>
2022-10-20 22:07:25 +0200

    intial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import Navbar from "./Navbar";
+
+export default function Home() {
+  return (
+    <div className="bg-[url(@assets/zen.jpg)] bg-cover min-h-screen w-screen">
+      <Navbar />
+    </div>
+  );
+}
